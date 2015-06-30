33da4a0ad83394a2826692f39561e95891c0c19c
Schuyler Summers <schuyler.mail@gmail.com>
2015-06-30 15:35:58 -0600

    Added mock controllers, controller interfaces, and controller factories to make it easier to work on this at the same time

---
+++ src/main/java/WishList/Controller/Interfaces/WishListControllerFactory.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package WishList.Controller.Interfaces;
+
+import WishList.Controller.MockControllers.MockWishListController;
+
+/**
+ *
+ * @author Schuyler
+ */
+public class WishListControllerFactory {
+    public WishListController getWishListController() {
+        return new MockWishListController();
+    }
+}
