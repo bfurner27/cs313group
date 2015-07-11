8235b35fab74b52e8a963a9ea4e0e45bbb18e19b
Ben Furner <bfurner27@yahoo.com>
2015-07-11 16:10:13 -0600

    changed getEnv to getenv

--- src/main/java/WishList/Model/DatabaseAccessor.java
+++ src/main/java/WishList/Model/DatabaseAccessor.java
@@ -49,8 +49,8 @@ public class DatabaseAccessor {
             }
             else {
                 DB_URL = "jdbc:mysql://" + dbHost + ":" + dbPort + "/java";
-                USER = System.getEnv("OPENSHIFT_MYSQL_DB_USERNAME");
-                PASS = System.getEnv("OPENSHIFT_MYSQL_DB_PASSWORD");
+                USER = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+                PASS = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
             }
         }
     }
